de1412334d97142c9056d1364c89bda0063df4e7
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2022-09-25 18:10:16 +0800

    with print

--- routes/custom/requisitioner.php
+++ routes/custom/requisitioner.php
@@ -7,6 +7,7 @@ use App\Http\Livewire\Requisitioner\TransactionsIndex;
 use App\Http\Livewire\Requisitioner\TravelOrders\TravelOrdersCreate;
 use App\Http\Livewire\Requisitioner\TravelOrders\TravelOrdersIndex;
 use App\Http\Livewire\Requisitioner\TravelOrders\TravelOrdersShow;
+use App\Http\Livewire\Requisitioner\TravelOrders\TravelOrdersView;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([
@@ -20,6 +21,7 @@ Route::middleware([
     Route::get('/travel-orders/create', TravelOrdersCreate::class)->name('travel-orders.create');
     Route::get('/travel-orders', TravelOrdersIndex::class)->name('travel-orders.index');
     Route::get('/travel-orders/{travel_order}', TravelOrdersShow::class)->name('travel-orders.show');
+    Route::get('/travel-orders/view/{travel_order}', TravelOrdersView::class)->name('travel-orders.view'); 
     Route::get('/itenerary/create', IteneraryCreate::class)->name('itenerary.create');
     Route::get('/itenerary/{itenerary}', IteneraryShow::class)->name('itenerary.show');
     Route::get('/disbursement-vouchers/{voucher_subtype}/create', DisbursementVouchersCreate::class)->name('disbursement-vouchers.create');
