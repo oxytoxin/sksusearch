7a8e5fe768c03719a7853697a7e30446cd513a6a
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2022-09-25 21:02:30 +0800

    travel order side nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,9 +73,37 @@
                                         <a href="#" class="flex items-center w-full py-2 pl-10 pr-2 text-sm font-medium rounded-md text-primary-600 group hover:bg-primary-100 hover:text-primary-900">
                                             Closed Disbursement Vouchers</a>
                                     </div>
-                                    <x-sidenav-link href="{{ route('requisitioner.travel-orders.index') }}" :active="request()->routeIs('requisitioner.travel-orders.index')">
+                                    </div>
+                                       
+                                     <div class="space-y-1" x-data="{ open: false }">
+                                    <button x-on:click="open=!open" type="button"
+                                        class="flex items-center w-full py-2 pr-2 text-sm font-medium text-left bg-white rounded-md text-primary-600 group hover:bg-primary-50 hover:text-primary-900 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                                        aria-controls="sub-menu-1" aria-expanded="false">
+                                        <!-- Expanded: "text-primary-400 rotate-90", Collapsed: "text-primary-300" -->
+                                        <svg :class="open ?
+                                            'rotate-90 flex-shrink-0 w-5 h-5 mr-2 text-primary-300 transition-colors duration-150 ease-in-out transform group-hover:text-primary-400' :
+                                            'flex-shrink-0 w-5 h-5 mr-2 text-primary-300 transition-colors duration-150 ease-in-out transform group-hover:text-primary-400'"
+                                            class="" viewBox="0 0 20 20" aria-hidden="true">
+                                            <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
+                                        </svg>
                                         Travel Orders
-                                    </x-sidenav-link>
+                                    </button>
+                                    <!-- Expandable link section, show/hide based on state. -->
+                                    <div class="space-y-1" id="sub-menu-1" x-show = 'open' 
+                                    x-transition:enter = 'transition ease-out duration-300'
+                                    x-transition:enter-start =  'opacity-0 scale-95'
+                                    x-transition:enter-end =  'opacity-100 scale-100'
+                                    x-transition:leave =  'transition ease-in duration-300'
+                                    x-transition:leave-start = 'opacity-100 scale-100' 
+                                    x-transition:leave-end = 'opacity-0 scale-95' class="origin-top-left">
+                                     <a href="{{ route('requisitioner.travel-orders.index') }}" class="flex items-center w-full py-2 pl-10 pr-2 text-sm font-medium rounded-md text-primary-600 group hover:bg-primary-100 hover:text-primary-900">
+                                            My Travel Orders</a>
+                                        <a href="#" class="flex items-center w-full py-2 pl-10 pr-2 text-sm font-medium rounded-md text-primary-600 group hover:bg-primary-100 hover:text-primary-900">
+                                            Pending Travel Orders</a>
+                                        <a href="#" class="flex items-center w-full py-2 pl-10 pr-2 text-sm font-medium rounded-md text-primary-600 group hover:bg-primary-100 hover:text-primary-900">
+                                            Approved Travel Orders</a>
+                                    </div>
+                                    </div>
                                 </div>
                             </nav>
                         </div>
