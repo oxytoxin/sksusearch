4a831a4696b434a0c09aeb4cc027c762e6fb18d9
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-09-28 20:23:03 +0800

    progress x color  red return

--- resources/views/components/disbursement_vouchers/disbursement_voucher_progress.blade.php
+++ resources/views/components/disbursement_vouchers/disbursement_voucher_progress.blade.php
@@ -1,16 +1,20 @@
 <div>
-    <ul>
+    <ul class="space-y-1">
         <li class="flex gap-2">
-            <x-ri-checkbox-circle-fill />
+            <x-ri-checkbox-circle-fill class="text-indigo-600" />
             <span>Disbursement Voucher created.</span>
         </li>
         @foreach ($steps as $step)
             <li class="flex gap-2">
-                @if ($disbursement_voucher->current_step_id >= $step->id || $disbursement_voucher->previous_step_id >= $step->id)
-                    <x-ri-checkbox-circle-fill />
+                @if   ( $disbursement_voucher->previous_step_id == $step->id)
+                    <x-ri-close-circle-fill  class="text-red-500" />
+                    
+                @elseif  ($disbursement_voucher->current_step_id >= $step->id || $disbursement_voucher->previous_step_id >= $step->id)
+                    <x-ri-checkbox-circle-fill class="text-indigo-600" />
                 @endif
-                <span>{{ implode(' ', [$step->process, $step->recipient, $step->sender]) }}</span>
+                    <span>{{ implode(' ', [$step->process, $step->recipient, $step->sender]) }}</span>
             </li>
         @endforeach
+        
     </ul>
 </div>
