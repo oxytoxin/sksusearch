850d47ca69d0d1c3b1478eed0796fbcf39a610d1
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-10-03 11:59:19 +0800

    create employee error
    exception thrown when user already exist in db

--- app/Filament/Resources/EmployeeInformationResource/Pages/CreateEmployeeInformation.php
+++ app/Filament/Resources/EmployeeInformationResource/Pages/CreateEmployeeInformation.php
@@ -5,13 +5,17 @@ namespace App\Filament\Resources\EmployeeInformationResource\Pages;
 use App\Filament\Resources\EmployeeInformationResource;
 use App\Models\EmployeeInformation;
 use App\Models\User;
+use Filament\Notifications\Notification;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Hash;
 
+
 class CreateEmployeeInformation extends CreateRecord
 {
+    // use Actions;
+    public $employee;
     protected static string $resource = EmployeeInformationResource::class;
 
     protected function getRedirectUrl(): string
@@ -19,26 +23,33 @@ class CreateEmployeeInformation extends CreateRecord
         return $this->getResource()::getUrl('index');
     }
 
-    protected function handleRecordCreation(array $data): Model
+    protected function handleRecordCreation(array $data) : Model
     {
         $user = User::create([
             'email' => $data['email'],
             'password' => Hash::make(strtolower(str_replace(" ", "", $data['last_name'] . "123"))),
         ]);
-
-
-        $employee = EmployeeInformation::create([
-            'first_name' => strtoupper($data['first_name']),
-            'last_name' => strtoupper($data['last_name']),
-            'full_name' => strtoupper($data['full_name']),
-            'address' =>  $data['address'],
-            'birthday' => $data['birthday'],
-            'user_id' => $user['id'],
-            'role_id' => $data['role_id'],
-            'position_id' => $data['position_id'],
-            'office_id' => $data['office_id'],
-
-        ]);
-        return $employee;
+        if(EmployeeInformation::where('full_name',strtoupper($data['full_name']))->exists()){
+            
+            // Notification::make()
+            //     ->title('User Already Exist.')
+            //     ->success()  
+            //     ->send();
+            return EmployeeInformation::where('full_name',strtoupper($data['full_name']))->getModel();
+        }else{
+            $employee = EmployeeInformation::create([
+                'first_name' => strtoupper($data['first_name']),
+                'last_name' => strtoupper($data['last_name']),
+                'full_name' => strtoupper($data['full_name']),
+                'address' =>  $data['address'],
+                'birthday' => $data['birthday'],
+                'user_id' => $user['id'],
+                'role_id' => $data['role_id'],
+                'position_id' => $data['position_id'],
+                'office_id' => $data['office_id'],
+            ]);
+            return $this->employee;
+        }
+        
     }
 }
