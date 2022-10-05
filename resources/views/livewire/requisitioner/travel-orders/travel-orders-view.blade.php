79fc5657c3d83f9ff06952193832341b2d42b4df
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2022-10-05 15:47:57 +0800

    time

--- resources/views/livewire/requisitioner/travel-orders/travel-orders-view.blade.php
+++ resources/views/livewire/requisitioner/travel-orders/travel-orders-view.blade.php
@@ -1,13 +1,16 @@
 <div class="">
     <div class="grid grid-cols-1 lg:grid-cols-3">
         <div class="flex-row col-span-1 lg:col-span-2">
-            <div class="px-4 py-5 bg-white border-b rounded-md border-primary-200 sm:px-6 md:rounded-none md:rounded-tl-lg">
+            <div
+                class="px-4 py-5 bg-white border-b rounded-md border-primary-200 sm:px-6 md:rounded-none md:rounded-tl-lg">
                 <div class="flex flex-wrap items-center justify-between -mt-4 -ml-4 sm:flex-nowrap">
                     <div class="w-full mt-4 ml-4">
                         <h3 class="text-lg font-medium leading-6 text-primary-900">Travel Order Details</h3>
                         <p class="mt-4 text-sm text-primary-500">Tracking Code: {{ $travel_order->tracking_code }}</p>
-                        <p class="mt-1 text-sm text-primary-500">Travel Order Type: {{ $travel_order->travel_order_type->name }}</p>
-                        <p class="mt-1 text-sm text-primary-500">Date Range: {{ $travel_order->date_from->format('F d Y') }} to
+                        <p class="mt-1 text-sm text-primary-500">Travel Order Type:
+                            {{ $travel_order->travel_order_type->name }}</p>
+                        <p class="mt-1 text-sm text-primary-500">Date Range:
+                            {{ $travel_order->date_from->format('F d Y') }} to
                             {{ $travel_order->date_to->format('F d Y') }}</p>
                         @if ($travel_order->travel_order_type_id == 1)
                             @if ($travel_order->other_details == '')
@@ -16,7 +19,8 @@
                                     {{ $travel_order->philippine_province->province_description }},
                                     {{ $travel_order->philippine_region->region_description }}</p>
                             @else
-                                <p class="mt-1 text-sm text-primary-500">Destination: {{ $travel_order->other_details }},
+                                <p class="mt-1 text-sm text-primary-500">Destination:
+                                    {{ $travel_order->other_details }},
                                     {{ $travel_order->philippine_city->city_municipality_description }},
                                     {{ $travel_order->philippine_province->province_description }},
                                     {{ $travel_order->philippine_region->region_description }}</p>
@@ -27,12 +31,15 @@
 
                         @if ($travel_order->signatories()->firstWhere('is_approved', false) == null)
                             @if ($travel_order->itineraries()->firstWhere('user_id', auth()->id()))
-                                <a target="_blank" href="{{ route('requisitioner.itinerary.show', ['itinerary' => $travel_order->itineraries()->firstWhere('user_id', auth()->id())]) }}"
+                                <a target="_blank"
+                                    href="{{ route('requisitioner.itinerary.show', ['itinerary' => $travel_order->itineraries()->firstWhere('user_id', auth()->id())]) }}"
                                     class="flex float-right px-4 py-2 text-sm rounded-full bg-primary-600 text-primary-100 hover:text-primary-100 hover:bg-primary-900 active:ring-primary-700 w-fit active:ring-2 active:ring-offset-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-auto">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-5 h-auto">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                     </svg>
                                     <span class="pl-2">
                                         View Itinerary
@@ -41,8 +48,10 @@
                             @else
                                 <a target="_blank" href="{{ route('requisitioner.itinerary.create') }}"
                                     class="flex float-right px-4 py-2 text-sm rounded-full bg-primary-600 text-primary-100 hover:text-primary-100 hover:bg-primary-900 active:ring-primary-700 w-fit active:ring-2 active:ring-offset-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-auto">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-5 h-auto">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
                                     <span class="pl-2">
                                         Create Itinerary
@@ -54,16 +63,22 @@
                 </div>
             </div>
 
-            <div class="px-4 py-5 mt-5 bg-white border-b rounded-md border-primary-200 sm:px-6 md:rounded-none md:rounded-bl-lg">
+            <div
+                class="px-4 py-5 mt-5 bg-white border-b rounded-md border-primary-200 sm:px-6 md:rounded-none md:rounded-bl-lg">
                 <div class="flex flex-wrap items-center justify-between -mt-4 -ml-4 sm:flex-nowrap">
                     <div class="mt-4 ml-4">
                         <h3 class="text-lg font-medium leading-6 text-primary-900">Status</h3>
                         @foreach ($travel_order->signatories as $signatory)
-                            <p class="mt-4 text-sm text-primary-500">Signatory: {{ $signatory->employee_information->full_name }}</p>
+                            <p class="mt-4 text-sm text-primary-500">Signatory:
+                                {{ $signatory->employee_information->full_name }}</p>
                             <p class="mt-1 text-sm text-primary-500">Approval Status:
                                 {{ $signatory->pivot->is_approved ? 'Approved' : 'Pending' }}</p>
                             <p class="mt-1 text-sm text-primary-500">Date Approved:
-                                {{ $signatory->pivot->is_approved ? $signatory->pivot->updated_at->format('F d, Y') : 'Unavailable' }}</p>
+                                {{ $signatory->pivot->is_approved ? $signatory->pivot->updated_at->format('F d, Y h:i:s a') : 'Unavailable' }}
+                            </p>
+                            <p class="mt-1 text-sm text-primary-500">Time Approved:
+                                {{ $signatory->pivot->is_approved ? $signatory->pivot->updated_at->format('h:i:s a') : 'Unavailable' }}
+                            </p>
                         @endforeach
 
                     </div>
@@ -71,7 +86,8 @@
             </div>
         </div>
 
-        <div class="col-span-1 px-4 py-5 ml-4 bg-white border-b rounded-md border-primary-300 md:rounded-l-none md:rounded-r-3xl">
+        <div
+            class="col-span-1 px-4 py-5 ml-4 bg-white border-b rounded-md border-primary-300 md:rounded-l-none md:rounded-r-3xl">
             <div class="flow-root mt-6">
                 <ul role="list" class="-my-5 divide-y divide-primary-200">
                     <h3 class="font-semibold text-primary-600 text-md">Notes</h3>
@@ -82,8 +98,10 @@
                                     <span class="absolute inset-0" aria-hidden="true"></span>
                                     {{ $sidenote->user->employee_information->full_name }}
                                 </h3>
-                                <p class="mt-1 text-sm text-primary-600 line-clamp-2">{{ $sidenote->user->content }}</p>
-                                <p class="mt-1 text-sm text-right text-primary-300">{{ $sidenote->created_at->format('M d, Y') }}</p>
+                                <p class="mt-1 text-sm text-primary-600 line-clamp-2">{{ $sidenote->user->content }}
+                                </p>
+                                <p class="mt-1 text-sm text-right text-primary-300">
+                                    {{ $sidenote->created_at->format('M d, Y') }}</p>
                             </div>
                         </li>
                     @empty
@@ -100,7 +118,8 @@
             </div>
             @if ($travel_order->sidenotes->count())
                 <div class="mt-6">
-                    <a href="#" class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium bg-white border rounded-md shadow-sm text-primary-700 border-primary-300 hover:bg-primary-50">
+                    <a href="#"
+                        class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium bg-white border rounded-md shadow-sm text-primary-700 border-primary-300 hover:bg-primary-50">
                         View all</a>
                 </div>
             @endif
--- resources/views/livewire/signatory/travel-orders/travel-orders-to-sign-view.blade.php
+++ resources/views/livewire/signatory/travel-orders/travel-orders-to-sign-view.blade.php
@@ -5,7 +5,8 @@
     <div class="grid grid-cols-1 lg:grid-cols-3">
         <div class="flex-row col-span-1 lg:col-span-2">
 
-            <div class="px-4 py-5 bg-white border-b rounded-md border-primary-200 sm:px-6 md:rounded-none md:rounded-t-lg">
+            <div
+                class="px-4 py-5 bg-white border-b rounded-md border-primary-200 sm:px-6 md:rounded-none md:rounded-t-lg">
                 <div class="flex-wrap items-center justify-between block -mt-4 -ml-4 sm:flex-nowrap">
                     <div class="mt-4 ml-4">
                         <h3 class="text-lg font-medium leading-6 text-primary-900">Travel Order Details</h3>
@@ -30,7 +31,8 @@
                             @endif
                         @endif
                         <p class="mt-1 text-sm text-primary-500">Purpose: {{ $travel_order->purpose }}</p>
-                        <p class="mt-1 text-primary-500">Registration Fee: <span class="">{{ $travel_order->registration_amount > 0 ? $travel_order->registration_amount : 'N/A' }}</span>
+                        <p class="mt-1 text-primary-500">Registration Fee: <span
+                                class="">{{ $travel_order->registration_amount > 0 ? $travel_order->registration_amount : 'N/A' }}</span>
                         </p>
                         {{-- <p class="mt-1 text-primary-500">Total Amount: <span class="">{{ $total_amount }}</span>
 						</p> --}}
@@ -45,15 +47,21 @@
                             <div class="flex justify-between w-full">
                                 <span>&nbsp</span>
                                 <div class="flex space-x-3">
-                                    <button class="flex text-sm text-primary-600 hover:text-primary-400" wire:click.prevent="approve">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                                            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                                    <button class="flex text-sm text-primary-600 hover:text-primary-400"
+                                        wire:click.prevent="approve">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                            class="w-5 h-5">
+                                            <path fill-rule="evenodd"
+                                                d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                                clip-rule="evenodd" />
                                         </svg>
                                         <span class="">Approve Travel Order</span>
                                     </button>
                                     <button class="flex text-sm text-red-500 hover:text-red-300">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                                            <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                            class="w-5 h-5">
+                                            <path
+                                                d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
                                         </svg>
                                         <span class="">Reject Travel Order</span>
                                     </button>
@@ -65,7 +73,8 @@
                 </div>
             </div>
 
-            <div class="px-4 py-5 mt-2 bg-white border-b rounded-md border-primary-200 sm:px-6 md:rounded-none md:rounded-b-lg">
+            <div
+                class="px-4 py-5 mt-2 bg-white border-b rounded-md border-primary-200 sm:px-6 md:rounded-none md:rounded-b-lg">
                 <div class="flex flex-wrap items-center justify-between -mt-4 -ml-4 sm:flex-nowrap">
                     <div class="mt-4 ml-4">
                         <h3 class="text-lg font-medium leading-6 text-primary-900"></h3>
@@ -77,6 +86,9 @@
                             <p class="mt-1 text-sm text-primary-500">Date Approved:
                                 {{ $signatory->pivot->is_approved ? $signatory->pivot->updated_at->format('F d, Y') : 'Unavailable' }}
                             </p>
+                            <p class="mt-1 text-sm text-primary-500">Time Approved:
+                                {{ $signatory->pivot->is_approved ? $signatory->pivot->updated_at->format('h:i:s a') : 'Unavailable' }}
+                            </p>
                         @endforeach
 
                     </div>
@@ -84,14 +96,16 @@
             </div>
         </div>
 
-        <div class="col-span-1 px-4 py-5 ml-4 overflow-y-auto bg-white border-b rounded-md border-primary-300 max-h-screen-70 soft-scrollbar">
+        <div
+            class="col-span-1 px-4 py-5 ml-4 overflow-y-auto bg-white border-b rounded-md border-primary-300 max-h-screen-70 soft-scrollbar">
             <div class="flow-root mt-6">
                 <ul role="list" class="-my-5 divide-y divide-primary-200">
                     <div class="flex justify-between w-full">
                         <h3 class="text-lg font-semibold text-primary-600">Notes</h3>
                         <button type="button" wire:click="$set('modal',true)" class="flex px-4 my-auto text-center">
                             <span class="my-auto text-sm"> Add note</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-auto h-3 my-auto">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="w-auto h-3 my-auto">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
                         </button>
@@ -104,7 +118,8 @@
                                 <div class="relative focus-within:ring-2 focus-within:ring-indigo-500">
                                     <h3 class="flex justify-between text-sm font-semibold text-primary-800">
                                         <span class="absolute inset-0" aria-hidden="true"></span>
-                                        <span class="flex">{{ $sidenote->user->employee_information->full_name }}</span>
+                                        <span
+                                            class="flex">{{ $sidenote->user->employee_information->full_name }}</span>
                                         <span class="flex">{{ $sidenote->created_at->format('M d, Y') }}</span>
                                     </h3>
                                     <p class="mt-1 text-sm text-primary-600 line-clamp-2">
@@ -127,7 +142,8 @@
             </div>
             @if ($travel_order->sidenotes->count() > $limit)
                 <div class="mt-6">
-                    <button type="button" wire:click="showMore()" class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium bg-white border rounded-md shadow-sm text-primary-700 border-primary-300 hover:bg-primary-50">View
+                    <button type="button" wire:click="showMore()"
+                        class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium bg-white border rounded-md shadow-sm text-primary-700 border-primary-300 hover:bg-primary-50">View
                         more</button>
                 </div>
             @endif
@@ -137,7 +153,8 @@
 
     <x-modal.card title="Add Note" blur wire:model.defer="modal" class="text-primary-600">
         <form wire:submit.prevent='addNote' class="space=y=2 flex-col">
-            <x-textarea class="text-primary-800 placeholder:text-primary-200" label="Your notes" placeholder="Write your notes" wire:model.defer="note" />
+            <x-textarea class="text-primary-800 placeholder:text-primary-200" label="Your notes"
+                placeholder="Write your notes" wire:model.defer="note" />
             <x-button type="submit" class="mt-2 text-primary-900 border-primary-800">
                 <span class="text-primary-900">
                     Save Note
