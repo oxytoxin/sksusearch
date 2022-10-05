1900e1e7d113b892c9d4e7c3073adedb3aed7a6f
oxytoxin <oxytoxinsgrace@gmail.com>
2022-10-05 14:45:00 +0800

    laravel db snapshots

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,15 +39,20 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
+        ],
+
+        'snapshots' => [
+            'driver' => 'local',
+            'root' => database_path('snapshots'),
         ],
 
         'remarks' => [
             'driver' => 'local',
             'root' => storage_path('app/public/remarks'),
-            'url' => env('APP_URL').'/storage/remarks',
+            'url' => env('APP_URL') . '/storage/remarks',
             'visibility' => 'public',
             'throw' => false,
         ],
