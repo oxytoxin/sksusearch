d970c81b23c0f86c168cfa7b14402f5db7bee424
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2022-10-05 22:42:38 +0800

    ok

--- app/Filament/Resources/CityResource/Pages/CreateCity.php
+++ app/Filament/Resources/CityResource/Pages/CreateCity.php
@@ -3,10 +3,36 @@
 namespace App\Filament\Resources\CityResource\Pages;
 
 use App\Filament\Resources\CityResource;
+use App\Models\PhilippineCity;
+use App\Models\PhilippineProvince;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateCity extends CreateRecord
 {
     protected static string $resource = CityResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $province_code = PhilippineProvince::where('province_code', $data['province_code'])->first();
+        $region_code = $province_code->region_code;
+        $city_code = rand(pow(10, 4 - 1), pow(10, 4) - 1);
+        $psgc = $city_code . '00000';
+
+        $city = PhilippineCity::create([
+            'psgc_code' => $psgc,
+            'city_municipality_description' => $data['city_municipality_description'],
+            'region_description' =>  $region_code,
+            'province_code' => $data['province_code'],
+            'city_municipality_code' => $city_code,
+        ]);
+
+        return $city;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/ProvinceResource/Pages/CreateProvince.php
+++ app/Filament/Resources/ProvinceResource/Pages/CreateProvince.php
@@ -3,12 +3,30 @@
 namespace App\Filament\Resources\ProvinceResource\Pages;
 
 use App\Filament\Resources\ProvinceResource;
+use App\Models\PhilippineProvince;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateProvince extends CreateRecord
 {
     protected static string $resource = ProvinceResource::class;
+
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $province_code = rand(pow(10, 4 - 1), pow(10, 4) - 1);
+        $psgc = $province_code . '00000';
+
+        $province = PhilippineProvince::create([
+            'psgc_code' => $psgc,
+            'province_description' => $data['province_description'],
+            'region_code' => $data['region_code'],
+            'province_code' => $province_code
+        ]);
+
+        return $province;
+    }
 
     protected function getRedirectUrl(): string
     {
