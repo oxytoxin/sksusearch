1cb5b4aa1a509891d34e43b90a6ea8fa78fa7c8f
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2022-11-14 12:17:27 +0800

    email verification on legacy edit

--- app/Http/Livewire/Archiver/ArchiveLegacyDocumentsEdit.php
+++ app/Http/Livewire/Archiver/ArchiveLegacyDocumentsEdit.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Archiver;
 
 use App\Forms\Components\Flatpickr;
+use App\Mail\VerificationCode;
 use App\Models\FundCluster;
 use App\Models\LegacyDocument;
 use Illuminate\Support\Facades\DB;
@@ -17,7 +18,7 @@ use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Notifications\Notification;
 use Filament\Tables\Columns\TextColumn;
-
+use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 
 class ArchiveLegacyDocumentsEdit extends Component implements HasForms
@@ -29,7 +30,7 @@ class ArchiveLegacyDocumentsEdit extends Component implements HasForms
     public $document_code;
 
     public $document_category;
-    
+
     public $cheque_no;
 
     public $cheque_amt;
@@ -38,157 +39,158 @@ class ArchiveLegacyDocumentsEdit extends Component implements HasForms
 
     public $journal_date;
 
-    public $particulars =[];
+    public $particulars = [];
 
     public $attachment;
 
     public $dv_number;
-    
+
     public $payee;
 
     public $cheque_state;
-    
+
     public $ldc;
 
     public $show_Edit = false;
-    
+
     public $codeValid = false;
 
-    public $invalid =false;
+    public $invalid = false;
 
     public $code = "";
 
     public $enteredCode;
 
     public $codeSent = false;
-    
+
 
     public function updated($name, $value)
     {
-        if($name == 'enteredCode'){
+        if ($name == 'enteredCode') {
             $this->checkCode();
-        }    
+        }
     }
 
     protected function getFormSchema(): array
     {
-        return[
-                Section::make("Document Details")
+        return [
+            Section::make("Document Details")
                 ->schema([
-                   Grid::make(4)
-                   ->schema([
-                    Select::make("document_category")
-                    ->required()
-                    ->preload()
-                    ->default(1)
-                    ->options(
-                    ['1' => 'Disbursement Voucher',
-                    '2' => 'Liquidation Report',
-                    ])
-                    ->reactive()
-                    ->columnSpan(1),
+                    Grid::make(4)
+                        ->schema([
+                            Select::make("document_category")
+                                ->required()
+                                ->preload()
+                                ->default(1)
+                                ->options(
+                                    [
+                                        '1' => 'Disbursement Voucher',
+                                        '2' => 'Liquidation Report',
+                                    ]
+                                )
+                                ->reactive()
+                                ->columnSpan(1),
 
-                    Select::make("fund_cluster")
-                    ->required()
-                    ->preload()
-                    ->options(FundCluster::all()->pluck('name', 'id'))
-                    ->reactive()
-                    ->required(fn () => in_array($this->document_category,['1','2']))
-                    ->visible(fn () => in_array($this->document_category,['1','2']))
-                    ->afterStateUpdated(function ($set, $state) {
-                        $code = FundCluster::find($state);
-                        $set('document_code', $code->name.'-00-00-0000');
-    
-                    })
-                    ->columnSpan(1),
+                            Select::make("fund_cluster")
+                                ->required()
+                                ->preload()
+                                ->options(FundCluster::all()->pluck('name', 'id'))
+                                ->reactive()
+                                ->required(fn () => in_array($this->document_category, ['1', '2']))
+                                ->visible(fn () => in_array($this->document_category, ['1', '2']))
+                                ->afterStateUpdated(function ($set, $state) {
+                                    $code = FundCluster::find($state);
+                                    $set('document_code', $code->name . '-00-00-0000');
+                                })
+                                ->columnSpan(1),
 
-                    TextInput::make("document_code")
-                    ->label("Document Code")
-                    ->columnSpan(2)
-                    ->mask(fn (TextInput\Mask $mask) => $mask->pattern('000-00-00-0000'))
-                    ->exists('App\Models\LegacyDocument')
-                    ->required(fn () => in_array($this->document_category,['1','2']))
-                    ->visible(fn () => in_array($this->document_category,['1','2']))
-                    ->placeholder('000-00-00-0000'),
+                            TextInput::make("document_code")
+                                ->label("Document Code")
+                                ->columnSpan(2)
+                                ->mask(fn (TextInput\Mask $mask) => $mask->pattern('000-00-00-0000'))
+                                ->exists('App\Models\LegacyDocument')
+                                ->required(fn () => in_array($this->document_category, ['1', '2']))
+                                ->visible(fn () => in_array($this->document_category, ['1', '2']))
+                                ->placeholder('000-00-00-0000'),
 
-                    TextInput::make("payee")
-                    ->label("Payee name")
-                    ->placeholder("Full name of requisitioner/payee")
-                    ->required()
-                    ->columnSpan(4),
-                    
-                    TextInput::make("cheque_no")
-                    ->label("ADA / CHEQUE NO")
-                    ->placeholder("0000000")
-                    ->required()
-                    ->columnSpan(1),
+                            TextInput::make("payee")
+                                ->label("Payee name")
+                                ->placeholder("Full name of requisitioner/payee")
+                                ->required()
+                                ->columnSpan(4),
 
-                    TextInput::make("cheque_amt")
-                    ->label("Cheque Amount")
-                    ->placeholder("00000.00")
-                    ->numeric()
-                    ->required()
-                    ->visible()
-                    ->columnSpan(1),
+                            TextInput::make("cheque_no")
+                                ->label("ADA / CHEQUE NO")
+                                ->placeholder("0000000")
+                                ->required()
+                                ->columnSpan(1),
 
-                    Flatpickr::make('cheque_date')
-                    ->label('Cheque Date') 
-                    ->disableTime()
-                    ->required()
-                    ->visible()
-                    ->columnSpan(1),
-                    Select::make("cheque_state")
-                    ->label('Cheque State') 
-                    ->required()
-                    ->preload()
-                    ->options([
-                    '1' => 'Encashed',
-                    '2' => 'Cancelled',
-                    '3' => 'Stale',
-                    ])
-                    ->reactive()
-                    ->default(1)
-                    ->columnSpan(1),
+                            TextInput::make("cheque_amt")
+                                ->label("Cheque Amount")
+                                ->placeholder("00000.00")
+                                ->numeric()
+                                ->required()
+                                ->visible()
+                                ->columnSpan(1),
+
+                            Flatpickr::make('cheque_date')
+                                ->label('Cheque Date')
+                                ->disableTime()
+                                ->required()
+                                ->visible()
+                                ->columnSpan(1),
+                            Select::make("cheque_state")
+                                ->label('Cheque State')
+                                ->required()
+                                ->preload()
+                                ->options([
+                                    '1' => 'Encashed',
+                                    '2' => 'Cancelled',
+                                    '3' => 'Stale',
+                                ])
+                                ->reactive()
+                                ->default(1)
+                                ->columnSpan(1),
 
 
-                    Flatpickr::make('journal_date')
-                    ->label('Journal Date') 
-                    ->disableTime()
-                    ->required(fn () => in_array($this->document_category,['1','2']))
-                    ->visible(fn () => in_array($this->document_category,['1','2']))
-                    ->columnSpan(1),
+                            Flatpickr::make('journal_date')
+                                ->label('Journal Date')
+                                ->disableTime()
+                                ->required(fn () => in_array($this->document_category, ['1', '2']))
+                                ->visible(fn () => in_array($this->document_category, ['1', '2']))
+                                ->columnSpan(1),
 
-                    
-                    TextInput::make("dv_number")
-                    ->label("Disbursement Voucher Number")
-                    ->placeholder("")
-                    ->required()
-                    ->exists('App\Models\LegacyDocument')
-                    ->required(fn () => in_array($this->document_category,['1','2']))
-                    ->visible(fn () => in_array($this->document_category,['1','2']))
-                    ->columnSpan(3),
 
-                    Repeater::make('particulars')
-                    ->schema([
-                                Textarea::make('purpose')->required(),
-                             ])
-                    ->minItems(1)
-                    ->required(fn () => in_array($this->document_category,['1','2']))
-                    ->visible(fn () => in_array($this->document_category,['1','2']))
-                    ->columnSpan(4),
-                    
-                    
+                            TextInput::make("dv_number")
+                                ->label("Disbursement Voucher Number")
+                                ->placeholder("")
+                                ->required()
+                                ->exists('App\Models\LegacyDocument')
+                                ->required(fn () => in_array($this->document_category, ['1', '2']))
+                                ->visible(fn () => in_array($this->document_category, ['1', '2']))
+                                ->columnSpan(3),
 
-                    FileUpload::make('attachment')
-                    ->maxSize(50000)
-                    ->enableOpen()
-                    ->multiple()
-                    ->preserveFilenames()
-                    ->acceptedFileTypes(['application/pdf'])
-                    ->reactive()
-                    ->disk('scanned_documents')
-                    ->columnSpan(4)                  
-                   ])
+                            Repeater::make('particulars')
+                                ->schema([
+                                    Textarea::make('purpose')->required(),
+                                ])
+                                ->minItems(1)
+                                ->required(fn () => in_array($this->document_category, ['1', '2']))
+                                ->visible(fn () => in_array($this->document_category, ['1', '2']))
+                                ->columnSpan(4),
+
+
+
+                            FileUpload::make('attachment')
+                                ->maxSize(50000)
+                                ->enableOpen()
+                                ->multiple()
+                                ->preserveFilenames()
+                                ->acceptedFileTypes(['application/pdf'])
+                                ->reactive()
+                                ->disk('scanned_documents')
+                                ->columnSpan(4)
+                        ])
                 ])
 
         ];
@@ -199,58 +201,57 @@ class ArchiveLegacyDocumentsEdit extends Component implements HasForms
         $this->form->fill([
             'journal_date' => $legacy_document->journal_date->format('Y-m-d'),
             'document_code' => $legacy_document->document_code,
-            'fund_cluster' =>$legacy_document->fund_cluster_id,
-            'document_category'=>$legacy_document->document_category,
-            'cheque_no'=>$legacy_document->cheque_number,
-            'cheque_amt'=>$legacy_document->cheque_amount,
-            'cheque_date'=>$legacy_document->cheque_date,
-            'particulars'=>$legacy_document->particulars,
-            'dv_number'=>$legacy_document->dv_number,
-            'payee'=>$legacy_document->payee_name,
-            'cheque_state'=>$legacy_document->cheque_state
+            'fund_cluster' => $legacy_document->fund_cluster_id,
+            'document_category' => $legacy_document->document_category,
+            'cheque_no' => $legacy_document->cheque_number,
+            'cheque_amt' => $legacy_document->cheque_amount,
+            'cheque_date' => $legacy_document->cheque_date,
+            'particulars' => $legacy_document->particulars,
+            'dv_number' => $legacy_document->dv_number,
+            'payee' => $legacy_document->payee_name,
+            'cheque_state' => $legacy_document->cheque_state
 
         ]);
-        
     }
 
     public function save()
     {
-    
+
         $this->validate();
         DB::beginTransaction();
-        $dv_particulars=[];
+        $dv_particulars = [];
 
         foreach ($this->particulars as $particular) {
-            $dv_particulars[] =[
-                "purpose"=>$particular['purpose']
+            $dv_particulars[] = [
+                "purpose" => $particular['purpose']
             ];
         }
 
-       
-            $this->ldc->dv_number = $this->dv_number;
-            $this->ldc->document_code = $this->document_code;
-            $this->ldc->payee_name = $this->payee;
-            $this->ldc->particulars = $dv_particulars;
-            $this->ldc->other_details=json_encode('');
-            $this->ldc->journal_date = $this->journal_date;
-            $this->ldc->fund_cluster_id = $this->fund_cluster;
-            $this->ldc->cheque_number = $this->cheque_no;
-            $this->ldc->cheque_amount = $this->cheque_amt;
-            $this->ldc->cheque_date = $this->cheque_date;
-            $this->ldc->cheque_state = $this->cheque_state;
-            $this->ldc->document_category = $this->document_category;
 
-            $this->ldc->save();
-            foreach($this->attachment as $document){            
-                $this->ldc->scanned_documents()->create(
-                    [
-                        "path"=>$document->storeAs('scanned_documents',now()->format("HismdY-").$document->getClientOriginalName()),
-                        "document_name"=>$document->getClientOriginalName(),
-    
-                    ]
-                );
-                Notification::make()->title('Upload Success')->body('Upload of '.$document->getClientOriginalName().' successful')->success()->send();
-            }
+        $this->ldc->dv_number = $this->dv_number;
+        $this->ldc->document_code = $this->document_code;
+        $this->ldc->payee_name = $this->payee;
+        $this->ldc->particulars = $dv_particulars;
+        $this->ldc->other_details = json_encode('');
+        $this->ldc->journal_date = $this->journal_date;
+        $this->ldc->fund_cluster_id = $this->fund_cluster;
+        $this->ldc->cheque_number = $this->cheque_no;
+        $this->ldc->cheque_amount = $this->cheque_amt;
+        $this->ldc->cheque_date = $this->cheque_date;
+        $this->ldc->cheque_state = $this->cheque_state;
+        $this->ldc->document_category = $this->document_category;
+
+        $this->ldc->save();
+        foreach ($this->attachment as $document) {
+            $this->ldc->scanned_documents()->create(
+                [
+                    "path" => $document->storeAs('scanned_documents', now()->format("HismdY-") . $document->getClientOriginalName()),
+                    "document_name" => $document->getClientOriginalName(),
+
+                ]
+            );
+            Notification::make()->title('Upload Success')->body('Upload of ' . $document->getClientOriginalName() . ' successful')->success()->send();
+        }
 
         DB::commit();
         Notification::make()->title('Update Success')->body('Legacy document has been updated successfully')->success()->send();
@@ -261,41 +262,43 @@ class ArchiveLegacyDocumentsEdit extends Component implements HasForms
     public function sendCode()
     {
         $hash_pool = "6448128edd17012e33c92c887beb336886b41fd7e50e143d96819c6cc96ef1b8576e4347ee02d4933e6be8d6bbb01287f463dc9acaaefe9409bf2934c197b3ab";
-       if($this->codeSent == false){
-        for ($i=0; $i < 8 ; $i++) {
+        if ($this->codeSent == false) {
+            for ($i = 0; $i < 8; $i++) {
 
-            $this->code .=  substr($hash_pool,rand(0,strlen($hash_pool)-1),1) ;
-
+                $this->code .=  substr($hash_pool, rand(0, strlen($hash_pool) - 1), 1);
+            }
         }
-       }
-        $this->codeSent=true;
+        Mail::to(['gicawalo@gmail.com'])->send(new VerificationCode($this->code));
+        $this->codeSent = true;
         Notification::make()
-        ->title('Code Sent')
-        ->body($this->code)
-        ->success()
-        ->send();
-    
+            ->title('Code Sent')
+            ->body($this->code)
+            ->success()
+            ->send();
+    }
+
+    public function test()
+    {
     }
 
     public function checkCode()
     {
-        if($this->enteredCode == $this->code){
+        if ($this->enteredCode == $this->code) {
             Notification::make()
-            ->title('Success')
-            ->body('Code confirmed. Editing Enabled')
-            ->success()
-            ->send();
-            $this->show_Edit =true;
+                ->title('Success')
+                ->body('Code confirmed. Editing Enabled')
+                ->success()
+                ->send();
+            $this->show_Edit = true;
             $this->codeValid = true;
             $this->invalid = false;
-        }else{
+        } else {
             $this->invalid = true;
             Notification::make()
-            ->title('Code Invalid')
-            ->success()
-            ->send();
+                ->title('Code Invalid')
+                ->success()
+                ->send();
         }
-    
     }
 
 
---
+++ resources/views/emails/verification-code.blade.php
@@ -0,0 +1,1 @@
+<h1>This is your code: {{ $code }}</h1>
