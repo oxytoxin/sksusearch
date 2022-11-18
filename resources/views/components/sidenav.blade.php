36684f66aebd74e54a880ae71f30df0b22aa9078
HydraBruteforce001 <42317700+HydraBruteforce001@users.noreply.github.com>
2022-11-18 11:42:25 +0800

    Update sidenav.blade.php

--- resources/views/components/sidenav.blade.php
+++ resources/views/components/sidenav.blade.php
@@ -287,7 +287,7 @@
         </div>
         @php
             $motorpool_head = App\Models\Office::where('name','like', '%Motorpool%')->first();
-            dd($motorpool_head?->admin_user_id.' ?= '.auth()->user()->id);
+            dd($motorpool_head?->admin_user_id.' ?= '.auth()->user()->id.$motorpool_head);
         @endphp
 
         <div class="space-y-1" x-data="{ open: false }">
