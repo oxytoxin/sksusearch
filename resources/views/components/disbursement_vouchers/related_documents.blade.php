ead5b5f6a7684458056cb7072e9cf100dc20a2db
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2023-02-07 09:08:36 +0800

    added bullets to related docs

--- resources/views/components/disbursement_vouchers/related_documents.blade.php
+++ resources/views/components/disbursement_vouchers/related_documents.blade.php
@@ -2,7 +2,7 @@
     <div class="space-y-4">
         <p class="text-sm italic tracking-wider">Below are the list of related documents required. Please ensure all
             documents are complete and valid before proceeding.</p>
-        <ul>
+        <ul class="list-disc pl-5">
             @forelse ($voucher_subtype->related_documents_list?->documents ?? [] as $document)
                 <li>
                     {{ $document }}
