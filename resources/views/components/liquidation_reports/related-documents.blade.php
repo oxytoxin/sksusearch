9a7682fb93c373938aaa71b2d9098c1f1275853e
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2023-02-07 09:22:55 +0800

    related documents on lr added bullets

--- resources/views/components/liquidation_reports/related-documents.blade.php
+++ resources/views/components/liquidation_reports/related-documents.blade.php
@@ -3,7 +3,7 @@
         <div class="space-y-4">
             <p class="text-sm italic tracking-wider">Below are the list of related documents required. Please ensure all
                 documents are complete and valid before proceeding.</p>
-            <ul>
+            <ul class="list-disc pl-5">
                 @forelse ($voucher_subtype->related_documents_list?->liquidation_report_documents ?? [] as $document)
                     <li>
                         {{ $document }}
