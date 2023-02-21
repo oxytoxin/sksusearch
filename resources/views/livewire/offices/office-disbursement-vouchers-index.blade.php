8bddf257d2363b66202ec2db4a6e7a9b8415a8e5
oxytoxin <oxytoxinsgrace@gmail.com>
2023-02-22 00:03:46 +0800

    wip

--- resources/views/livewire/offices/office-disbursement-vouchers-index.blade.php
+++ resources/views/livewire/offices/office-disbursement-vouchers-index.blade.php
@@ -1,19 +1,19 @@
 <div class="mt-4">
-	<div class="w-full p-5 mb-2 bg-white border border-gray-300 shadow-sm rounded-xl">
-		
-		<div class="flex py-1 rounded-md">
-			<div class="w-full">
-				<div class="flex">
-					<input x-ref="tracking_num_from_scan" type="text" id="tracking_num_from_scan" class="w-full py-1 rounded-md"
-						name="tracking_num_from_scan" wire:model.debounce.750ms="tracking_num_from_scan" id="tracking_num_from_scan" placeholder="Click me to start Scan-to-Recieve">
-				</div>
-			</div>
-		</div>
-		<div class="flex">
-            <div class="ml-1">
-                <p class="text-blue-600"><strong class="italic">Note:</strong>  If it's not working, try clicking the text box above before scanning the qr. </p>
+    <div class="w-full p-5 mb-2 bg-white border border-gray-300 shadow-sm rounded-xl">
+
+        <div class="flex py-1 rounded-md">
+            <div class="w-full">
+                <div class="flex">
+                    <input class="w-full py-1 rounded-md" id="tracking_num_from_scan" id="tracking_num_from_scan" name="tracking_num_from_scan" type="text" x-ref="tracking_num_from_scan"
+                           wire:model.debounce.750ms="tracking_num_from_scan" placeholder="Click me to start Scan-to-Receive">
+                </div>
             </div>
-		</div>
-	</div>
-	{{ $this->table }}
+        </div>
+        <div class="flex">
+            <div class="ml-1">
+                <p class="text-blue-600"><strong class="italic">Note:</strong> If it's not working, try clicking the text box above before scanning the qr. </p>
+            </div>
+        </div>
+    </div>
+    {{ $this->table }}
 </div>
