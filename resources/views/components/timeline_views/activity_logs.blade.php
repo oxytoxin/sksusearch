9b90dcf638d86e0a3078889eb8d327e4fc57444b
oxytoxin <oxytoxinsgrace@gmail.com>
2023-03-02 17:30:01 +0800

    wip

--- resources/views/components/timeline_views/activity_logs.blade.php
+++ resources/views/components/timeline_views/activity_logs.blade.php
@@ -5,16 +5,14 @@
             <li>
                 <div class="flex gap-2">
                     <div>
-                        <div
-                            class="flex flex-col items-center justify-center p-2 text-xs bg-white rounded whitespace-nowrap">
+                        <div class="flex flex-col min-w-[8rem] items-center justify-center p-2 text-xs bg-white rounded whitespace-nowrap">
                             <p>{{ $log->created_at->format('h:i A') }}</p>
                             <p>{{ $log->created_at->format('F j, Y') }}</p>
                         </div>
                     </div>
                     <div class="flex-1">
                         <div class="flex items-center">
-                            <h4
-                                class="flex px-3 py-1 text-sm text-white whitespace-pre-line rounded md:rounded-full bg-primary-700">
+                            <h4 class="flex px-3 py-1 text-sm text-white whitespace-pre-line rounded md:rounded-full bg-primary-700">
                                 <p>{{ $log->description }}</p>
                             </h4>
                         </div>
