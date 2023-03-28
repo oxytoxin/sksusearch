6b1cb3801d38134e45e73134ec52df7116593974
oxytoxin <oxytoxinsgrace@gmail.com>
2023-03-28 15:18:51 +0800

    nav patch

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-4 sm:-my-px sm:ml-14 sm:flex ">
-                    @if (in_array(auth()->user()->employee_information?->office_id, [2, 3, 5, 25, 51, 52]))
+                    @if (auth()->user()->employee_information?->office->office_group_id != null)
                         <x-jet-nav-link href="{{ route('office.dashboard') }}" :active="request()->routeIs('office.dashboard')">
                             {{ __('Office Dashboard') }}
                         </x-jet-nav-link>
