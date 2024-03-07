ed9e7ba2332e0155866a3423a2d71f790f6ffb03
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-03-07 11:12:40 +0800

    added table

--- app/Http/Livewire/Archiver/ViewRecordCounts.php
+++ app/Http/Livewire/Archiver/ViewRecordCounts.php
@@ -10,12 +10,14 @@ class ViewRecordCounts extends Component
     public $init_count;
     public $legacy_document_years;
     public $year;
+    protected  $legacy_docs;
 
     public function mount()
     {
         $this->init_count = LegacyDocument::count();
         $this->legacy_document_years = LegacyDocument::selectRaw('YEAR(journal_date) year')->distinct()->orderBy('year', 'desc')->get();
         $this->year = 'all';
+        $this->legacy_docs = LegacyDocument::paginate(25);
     }
 
     public function updatedYear($value)
@@ -23,9 +25,11 @@ class ViewRecordCounts extends Component
         if($value == 'all'){
             $this->init_count = LegacyDocument::count();
             $this->legacy_document_years = LegacyDocument::selectRaw('YEAR(journal_date) year')->distinct()->orderBy('year', 'desc')->get();
+            $this->legacy_docs = LegacyDocument::paginate(25);
         }else{
             $this->init_count = LegacyDocument::whereYear('journal_date', $value)->count();
             $this->legacy_document_years = LegacyDocument::selectRaw('YEAR(journal_date) year')->distinct()->orderBy('year', 'desc')->get();
+            $this->legacy_docs = LegacyDocument::whereYear('journal_date', $value)->paginate(25);
         }
     }
 
@@ -35,6 +39,6 @@ class ViewRecordCounts extends Component
     }
     public function render()
     {
-        return view('livewire.archiver.view-record-counts');
+        return view('livewire.archiver.view-record-counts')->with('legacy_docs', $this->legacy_docs);
     }
 }
--- resources/views/livewire/archiver/view-record-counts.blade.php
+++ resources/views/livewire/archiver/view-record-counts.blade.php
@@ -2,7 +2,7 @@
     <div class="flex">
         <h2 class="font-light capitalize text-primary-600">Legacy Document Count</h2>
     </div>
-    <div class="w-1/2 rounded-lg bg-white p-4">
+    <div class="w-full rounded-lg bg-white p-4">
         <div class="flex justify-between">
             <div>
                 <x-filament-support::button wire:click="redirectBack">Back</x-filament-support::button>
@@ -16,8 +16,46 @@
                 </x-select>
             </div>
         </div>
-        <div class="mt-10 font-medium capitalize text-primary-600 text-lg text-center">
+        <div class="mt-10 font-medium capitalize text-primary-600 text-lg text-right">
             Total Count: {{$init_count}}
         </div>
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="mt-8 flow-root">
+              <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                  <table class="min-w-full divide-y divide-gray-300">
+                    <thead>
+                      <tr>
+                        <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Document Code</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">DV Number</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Payee</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Journal Date</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Cheque Date</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Fund Cluster</th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Document Category</th>
+                      </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200">
+                        @foreach ($legacy_docs as $doc)
+                        <tr>
+                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">{{$doc->document_code}}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$doc->dv_number}}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$doc->payee_name}}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{Carbon\Carbon::parse($doc->journal_date)->format('F d, Y')}}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{Carbon\Carbon::parse($doc->cheque_number)->format('F d, Y')}}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$doc->fund_cluster->name}}</td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$doc->document_category == 1 ? 'Disbursement Voucher' : 'Liquidation Report'}}</td>
+                          </tr>
+                        @endforeach
+
+                      <!-- More people... -->
+                    </tbody>
+                  </table>
+                  {{$legacy_docs->links()}}
+                </div>
+              </div>
+            </div>
+          </div>
+
     </div>
 </div>
