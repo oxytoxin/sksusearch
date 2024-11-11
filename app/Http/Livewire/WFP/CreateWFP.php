26939676a81aa1fae7a5da59a2ac56c311eaefa4
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-11-11 13:54:52 +0800

    a

--- app/Http/Livewire/WFP/CreateWFP.php
+++ app/Http/Livewire/WFP/CreateWFP.php
@@ -174,7 +174,7 @@ class CreateWFP extends Component implements Forms\Contracts\HasForms
 
     public function mount($record, $wfpType)
     {
-        $costCenter_id = Wfp::where('id', $record->id)->first()->cost_center_id;
+        $costCenter_id = Wfp::where('id', $record)->first()->cost_center_id;
         dd($costCenter_id);
         $this->wfp_param = $wfpType;
         $this->record = CostCenter::where('id', $record->id)->whereHas('fundAllocations', function ($query) use ($wfpType) {
