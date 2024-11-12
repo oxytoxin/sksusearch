c2f254e4b4f9bf98e1b2c3576d40e313cbfd629e
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-11-12 11:36:20 +0800

    hide ids

--- app/Filament/Resources/BudgetCategoryResource.php
+++ app/Filament/Resources/BudgetCategoryResource.php
@@ -41,7 +41,7 @@ class BudgetCategoryResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('id')->searchable()->sortable(),
+                // TextColumn::make('id')->searchable()->sortable(),
                 TextColumn::make('name')->searchable()->sortable(),
             ])
             ->filters([
