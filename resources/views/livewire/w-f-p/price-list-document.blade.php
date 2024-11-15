55c86645fccfedd03d60792fdf82f0f7fbeb3729
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-11-15 15:32:25 +0800

    removed svg

--- resources/views/livewire/w-f-p/price-list-document.blade.php
+++ resources/views/livewire/w-f-p/price-list-document.blade.php
@@ -3,9 +3,9 @@
         <h2 class="font-light capitalize text-primary-600">Pricelist Document</h2>
            <a href="{{ route('requisitioner.dashboard') }}"
             class="flex space-x-4 hover:bg-yellow-500 p-2 bg-yellow-600 rounded-md font-light capitalize text-white text-sm">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-              </svg>
+              </svg> --}}
               <span class="mt-1">Return</span>
             </a>
     </div>
