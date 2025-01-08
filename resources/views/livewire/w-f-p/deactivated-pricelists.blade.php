d8bbc908f029c89aec15d6ba8ae3fd09c5bced53
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2025-01-08 22:03:20 +0800

    removed table

--- resources/views/livewire/w-f-p/deactivated-pricelists.blade.php
+++ resources/views/livewire/w-f-p/deactivated-pricelists.blade.php
@@ -1,7 +1,7 @@
 <div class="space-y-2">
     <div class="flex justify-between items-center">
         <h2 class="font-light capitalize text-primary-600">Deactivated Pricelists</h2>
-        <a href="{{ route('wfp.wfp-submissions') }}"
+        <a href="{{ route('wfp.wfp-submissions', 1) }}"
             class="hover:bg-primary-500 p-2 bg-primary-600 rounded-md font-light capitalize text-white text-sm">Back</a>
     </div>
     <div>
@@ -9,7 +9,7 @@
             <div class="mt-8 flow-root">
               <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                  {{-- <table class="min-w-full divide-y divide-gray-500">
+                  <table class="min-w-full divide-y divide-gray-500">
                     <thead>
                       <tr class="divide-x divide-gray-500">
                         <th scope="col" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-0">Cost Center</th>
@@ -46,9 +46,9 @@
                           </tr>
                           @endforelse
                     </tbody>
-                  </table> --}}
+                  </table>
                 </div>
-                <div>
+                {{-- <div>
                   <button wire:click="updateAmounts" class="bg-green-600 p-4 text-gray-50 rounded-lg">Update</button>
                 </div>
                 <div>
@@ -56,8 +56,8 @@
                 </div>
                 <div>
                     <button wire:click="deleteItems" class="mt-4 bg-green-600 p-4 text-gray-50 rounded-lg">Delete Items</button>
-                  </div>
-                <table class="min-w-full divide-y divide-gray-500 mt-10">
+                  </div> --}}
+                {{-- <table class="min-w-full divide-y divide-gray-500 mt-10">
                   <thead>
                     <tr class="divide-x divide-gray-500">
                       <th scope="col" class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-0">Cost Center</th>
@@ -147,7 +147,7 @@
 
 
                   </tbody>
-                </table>
+                </table> --}}
 
               </div>
             </div>
