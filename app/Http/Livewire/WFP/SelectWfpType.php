60b08858724508313ef363ce656f5f73bfbb9ccf
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2025-01-12 21:54:11 +0800

    dd

--- app/Http/Livewire/WFP/SelectWfpType.php
+++ app/Http/Livewire/WFP/SelectWfpType.php
@@ -83,14 +83,11 @@ class SelectWfpType extends Component implements HasTable
         return CostCenter::query()->whereHas('fundAllocations', function ($query) {
             $query->where('is_locked', 1);
         })
-        ->where('fund_cluster_w_f_p_s_id', $this->fund_cluster)
         ->whereIn('id', $this->cost_centers->pluck('id')->toArray())
+        ->where('fund_cluster_w_f_p_s_id', $this->fund_cluster)
         ->orWhereHas('wpfPersonnel', function ($query) {
             $query->where('user_id', Auth::user()->id)
-            ->orWhere('head_id', Auth::user()->id)
-            ->whereHas('cost_center', function ($subQuery) {
-                $subQuery->where('fund_cluster_w_f_p_s_id', $this->fund_cluster);
-            });
+            ->orWhere('head_id', Auth::user()->id);
         });
         // if($user === null)
         // {
