448c0363227a5761f910e791395e13465b2ad9fb
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2025-01-13 14:42:35 +0800

    test

--- app/Http/Livewire/WFP/WFPHistory.php
+++ app/Http/Livewire/WFP/WFPHistory.php
@@ -28,10 +28,13 @@ class WFPHistory extends Component implements HasTable
 
     protected function getTableQuery()
     {
-        return Wfp::query()->whereIn('cost_center_id', $this->cost_centers->pluck('id')->toArray())
+        return Wfp::query()->where('user_id', Auth::id())
         ->orWhereHas('costCenter.wpfPersonnel', function ($query) {
             $query->where('user_id', Auth::user()->id)
-                ->orWhere('head_id', Auth::user()->id);
+                ->orWhere('head_id', Auth::user()->id)
+                ->whereHas('cost_center', function ($subQuery) {
+                    $subQuery->whereIn('cost_center_id', $this->cost_centers->pluck('id')->toArray());
+                });
         });
     }
 
