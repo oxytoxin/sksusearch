395bdd77d98d7e4a867d4096e15cae048984738e
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-01-21 14:48:31 +0800

    update query

--- app/Http/Livewire/WFP/UserPRE.php
+++ app/Http/Livewire/WFP/UserPRE.php
@@ -38,21 +38,6 @@ class UserPRE extends Component
             })
             ->get();
 
-
-            // $this->ppmp_details = WfpDetail::whereHas('wfp', function($query) {
-            //     $query->where('cost_center_id', $this->record->cost_center_id)->where('fund_cluster_w_f_p_s_id', $this->record->fund_cluster_w_f_p_s_id);
-            // })
-            // ->join('category_groups', 'wfp_details.category_group_id', '=', 'category_groups.id')
-            // ->join('supplies', 'wfp_details.supply_id', '=', 'supplies.id') // Join with the supplies table
-            // ->join('category_item_budgets', 'supplies.category_item_budget_id', '=', 'category_item_budgets.id') // Join with the category_items_budget table
-            // ->select('wfp_details.category_group_id',
-            // 'wfp_details.category_item_id',
-            // \DB::raw('SUM(cost_per_unit * total_quantity) as total_budget',
-            // 'category_item_budgets.uacs_code as budget_uacs', // Include the related field in the select
-            // 'category_item_budgets.name as budget_name')) // Include the related field in the select
-            // ->groupBy('category_group_id','category_item_id', 'category_item_budgets.uacs_code', 'category_item_budgets.name')
-            // ->get();
-
             $this->ppmp_details = WfpDetail::whereHas('wfp', function ($query) {
                 $query->where('cost_center_id', $this->record->cost_center_id)
                       ->where('fund_cluster_w_f_p_s_id', $this->record->fund_cluster_w_f_p_s_id);
@@ -88,6 +73,27 @@ class UserPRE extends Component
                       });
             })->get();
 
+            // $this->ppmp_details = WfpDetail::whereHas('wfp', function ($query) {
+            //     $query->where('cost_center_id', $this->record->cost_center_id)
+            //           ->where('fund_cluster_w_f_p_s_id', $this->record->fund_cluster_w_f_p_s_id);
+            // })
+            // ->join('wfps', 'wfp_details.wfp_id', '=', 'wfps.id') // Join with the wfp table
+            // ->join('supplies', 'wfp_details.supply_id', '=', 'supplies.id') // Join with the supplies table
+            // ->join('category_item_budgets', 'supplies.category_item_budget_id', '=', 'category_item_budgets.id')
+            // ->join('category_items', 'supplies.category_item_id', '=', 'category_items.id')
+            // ->select(
+            //     'wfps.cost_center_id as cost_center_id',
+            //     'category_items.uacs_code as uacs',
+            //     'category_items.name as item_name',
+            //     \DB::raw('SUM(wfp_details.cost_per_unit * wfp_details.total_quantity) as total_budget'),
+            //     'category_item_budgets.uacs_code as budget_uacs', // Include the related field in the select
+            //     'category_item_budgets.name as budget_name', // Include the related field in the select
+            //     'category_item_budgets.id as budget_id',
+            //     \DB::raw('SUM(wfp_details.cost_per_unit * wfp_details.total_quantity) as total_budget_per_uacs') // Total budget per budget_uacs and budget_name
+            // )
+            // ->groupBy('cost_center_id', 'uacs', 'budget_id', 'item_name', 'budget_uacs', 'budget_name')
+            // ->get();
+
             $this->ppmp_details = WfpDetail::whereHas('wfp', function ($query) {
                 $query->where('cost_center_id', $this->record->cost_center_id)
                       ->where('fund_cluster_w_f_p_s_id', $this->record->fund_cluster_w_f_p_s_id);
@@ -98,15 +104,16 @@ class UserPRE extends Component
             ->join('category_items', 'supplies.category_item_id', '=', 'category_items.id')
             ->select(
                 'wfps.cost_center_id as cost_center_id',
-                'category_items.uacs_code as uacs',
-                'category_items.name as item_name',
                 \DB::raw('SUM(wfp_details.cost_per_unit * wfp_details.total_quantity) as total_budget'),
                 'category_item_budgets.uacs_code as budget_uacs', // Include the related field in the select
                 'category_item_budgets.name as budget_name', // Include the related field in the select
                 \DB::raw('SUM(wfp_details.cost_per_unit * wfp_details.total_quantity) as total_budget_per_uacs') // Total budget per budget_uacs and budget_name
             )
-            ->groupBy('cost_center_id', 'uacs', 'item_name', 'budget_uacs', 'budget_name')
+            ->groupBy('cost_center_id', 'budget_uacs', 'budget_name')
             ->get();
+
+
+
 
 
             $this->total_allocated = FundAllocation::where('cost_center_id', $this->cost_center->id)->where('initial_amount', '>', 0)->sum('initial_amount');
