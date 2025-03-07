50b71da9c7530871aad34ecd3a80690b61ccc889
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-03-07 16:52:43 +0800

    p

--- app/Http/Livewire/Motorpool/Requests/FuelRequisition.php
+++ app/Http/Livewire/Motorpool/Requests/FuelRequisition.php
@@ -4,9 +4,16 @@ namespace App\Http\Livewire\Motorpool\Requests;
 
 use Livewire\Component;
 use App\Models\RequestSchedule;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use App\Models\Campus;
 
-class FuelRequisition extends Component
+class FuelRequisition extends Component implements HasForms
 {
+    use InteractsWithForms;
+
     public $request;
     public $showPrintable = false;
     public $selectedDate;
@@ -14,6 +21,19 @@ class FuelRequisition extends Component
     public function mount($request)
     {
         $this->request = RequestSchedule::find($request);
+    }
+
+
+    protected function getFormSchema()
+    {
+        return [
+            TextInput::make('model')->required(),
+            TextInput::make('plate_number')->required(),
+            Select::make('campus_id')
+                ->label('Campus')
+                ->options(Campus::pluck('name', 'id'))
+                ->required()
+        ];
     }
 
     public function render()
--- resources/views/livewire/motorpool/requests/fuel-requisition.blade.php
+++ resources/views/livewire/motorpool/requests/fuel-requisition.blade.php
@@ -1,3 +1,11 @@
 <div>
-    {{ $request }}
+        @for($x = 0; $x < 15; $x++)
+            <label for="">{{ $fuel_request_form }}</label>
+
+        {{ $request }}
+        @endfor
+
+    {{-- {{ $this->form }} --}}
+</div>
+<div>
 </div>
