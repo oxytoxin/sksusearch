e91a4abbdf802c83488f27dd4dee74c46f848ce1
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-05-20 16:14:12 +0800

    record

--- app/Http/Livewire/WFP/CreateWFP.php
+++ app/Http/Livewire/WFP/CreateWFP.php
@@ -367,7 +367,7 @@ class CreateWFP extends Component implements Forms\Contracts\HasForms
                 }else{
                     //balance 164
                     $programmed = [];
-                    if ($record->wfp !== null)
+                    if ($this->record->wfp !== null)
                     {
                         foreach($this->record->wfp->where('wpf_type_id', $wfpType)->where('cost_center_id', $this->record->id)->get() as $wfp)
                         {
