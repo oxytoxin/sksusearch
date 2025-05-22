a9358e52e3ae2d131e039bb32402226054e31309
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2025-05-22 11:33:49 +0800

    fix

--- app/Http/Livewire/WFP/SelectWfpType.php
+++ app/Http/Livewire/WFP/SelectWfpType.php
@@ -169,7 +169,7 @@ class SelectWfpType extends Component implements HasTable
             ->label('Create WFP')
             ->button()
             ->icon('heroicon-o-plus')
-            ->url(fn ($record): string => route('wfp.create-wfp', ['record' => $record, 'wfpType' => $this->data['wfp_type'], 'isEdit' => 0]))
+            ->url(fn ($record): string => route('wfp.create-wfp', ['record' => $record, 'wfpType' => $this->data['wfp_type'], 'isEdit' => 0, 'isSupplemental' => 0]))
             ->visible(fn ($record) => !$record->fundAllocations->where('wpf_type_id', $this->data['wfp_type'])->first()->fundDrafts()->exists()),
             Action::make('continue_draft')
             ->label('Continue Draft')
