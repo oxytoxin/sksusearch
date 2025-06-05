9be25ed9f1fa6a1127cb6fee46f7cc2c6eab233b
Jam <jamclong0328@gmail.com>
2025-06-05 10:40:08 +0800

    sync

--- resources/views/livewire/w-f-p/select-wfp-type-q1.blade.php
+++ resources/views/livewire/w-f-p/select-wfp-type-q1.blade.php
@@ -1,36 +1,40 @@
 <div>
     <div>
         @if ($wfp_type > 0)
-        <div x-data="{ selectedTab: '101' }">
+            <div x-data="{ selectedTab: '101' }">
 
-            <div class="sm:hidden">
-                <label for="tabs" class="sr-only">Select a tab</label>
-                <select id="tabs" name="tabs" class="block w-full rounded-md border-gray-300 focus:border-green-500 focus:ring-green-500"
-                    x-model="selectedTab">
-                    <option>101</option>
-                    <option>161</option>
-                    <option>163</option>
-                    <option>164T / FHE</option>
-                    <option>164T / Non-FHE</option>
-                    <option>164OSF</option>
-                    <option>164MF</option>
-                </select>
-            </div>
-            @php
-                $fund = App\Models\FundClusterWFP::get();
-            @endphp
-            <div class="hidden sm:block">
-                <nav class="flex space-x-4" aria-label="Tabs">
-                    <a wire:click="filter(1)" href="#"
-                       class="rounded-md px-3 py-2 text-sm font-medium"
-                       :class="{
-                           'bg-green-500 text-white': selectedTab === '101',
-                           'text-gray-800 hover:text-green-700': selectedTab !== '101'
-                       }"
-                       @click.prevent="selectedTab = '101'">
-                       Fund {{$fund->where('id', 1)->first()->name}}
-                    </a>
+                <div class="sm:hidden">
+                    <label for="tabs" class="sr-only">Select a tab</label>
+                    <select id="tabs" name="tabs"
+                        class="block w-full rounded-md border-gray-300 focus:border-green-500 focus:ring-green-500"
+                        x-model="selectedTab">
+                        <option>101</option>
+                        <option>161</option>
+                        <option>163</option>
+                        <option>164T / FHE</option>
+                        <option>164T / Non-FHE</option>
+                        <option>164OSF</option>
+                        <option>164MF</option>
+                    </select>
+                </div>
+                @php
+                    $funds = App\Models\FundClusterWFP::where('position', '!=', 0)->orderBy('position', 'asc')->get();
+                @endphp
+                <div class="hidden sm:block">
+                    <nav class="flex space-x-4" aria-label="Tabs">
+                        @foreach ($funds as $fund)
+                            <button wire:click="filter({{ $fund->id }})"
+                                class="rounded-md px-3 py-2 text-sm font-medium"
+                                :class="{
+                                    'bg-green-500 text-white': selectedTab === '{{ $fund->name }}',
+                                    'text-gray-800 hover:text-green-700': selectedTab !== '{{ $fund->name }}'
+                                }"
+                                @click.prevent="selectedTab = '{{ $fund->name }}'">
+                                Fund {{ $fund->name }}
+                            </button>
+                        @endforeach
 
+                        {{--
                     <a wire:click="filter(2)" href="#"
                        class="rounded-md px-3 py-2 text-sm font-medium"
                        :class="{
@@ -89,17 +93,17 @@
                        }"
                        @click.prevent="selectedTab = '164MF'">
                        Fund {{$fund->where('id', 6)->first()->name}}
-                    </a>
-                </nav>
+                    </a> --}}
+                    </nav>
+                </div>
             </div>
-        </div>
-        <div class="mt-4">
-            {{ $this->table }}
-        </div>
+            <div class="mt-4">
+                {{ $this->table }}
+            </div>
         @else
-        <div class="flex justify-center items-center h-64">
-            <h2 class="font-light text-gray-500">-- No WFP Period Added --</h2>
-        </div>
+            <div class="flex justify-center items-center h-64">
+                <h2 class="font-light text-gray-500">-- No WFP Period Added --</h2>
+            </div>
         @endif
     </div>
 </div>
