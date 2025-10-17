e3d66db10fd616c7c7cd97519ba664f779c4d8c1
Tristan Sarceda <orbinobrian0506@gmail.com>
2025-10-17 13:35:11 +0800

    udate formal amange demand date

--- resources/views/livewire/reports/formal-management-demand.blade.php
+++ resources/views/livewire/reports/formal-management-demand.blade.php
@@ -56,9 +56,14 @@
                         @endforeach
                     </ul>
                     <div class="mt-4 text-xs text-gray-800 leading-relaxed">
-                        <p>Records also show that a prior reminder contained in FMR No. xxxx-xxxx was issued to you on
-                            &lt;Date&gt; in
-                            relation hereto.</p>
+                    <p>
+    Records also show that a prior reminder contained in
+    FMR No. {{ $record->CaReminderStep->fmr_number ?? 'xxxx-xxxx' }}
+    was issued to you on
+{{ optional($record->CaReminderStep->fmr_date)->format('F d, Y') ?? '__________' }}
+    in relation hereto.
+</p>
+
 
                         <p class="mt-4">In view of the foregoing premises, you are hereby ordered to effect the
                             immediate
