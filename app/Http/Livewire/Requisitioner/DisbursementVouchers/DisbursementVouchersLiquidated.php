d39d1442c3106153a1c9913854509ed1cf315dc3
Tristan Sarceda <orbinobrian0506@gmail.com>
2025-11-06 10:12:40 +0800

    3123

--- app/Http/Livewire/Requisitioner/DisbursementVouchers/DisbursementVouchersLiquidated.php
+++ app/Http/Livewire/Requisitioner/DisbursementVouchers/DisbursementVouchersLiquidated.php
@@ -42,7 +42,7 @@ class DisbursementVouchersLiquidated extends Component implements HasTable
             Action::make('view')
                 ->button()
                 ->url(fn($record) => route('requisitioner.liquidation-reports.show', [
-                    'liquidation_report' => $record
+                    'liquidation_report' => $record->liquidation_report
                 ])),
             Action::make('view_notices')
                 ->label('View Notices')
