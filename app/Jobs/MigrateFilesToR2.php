87f88b74a36f9cda3a39290b11a451c3729b0522
xdd <jamclong0328@gmail.com>
2025-11-28 15:28:00 +0800

    Update MigrateFilesToR2.php

--- app/Jobs/MigrateFilesToR2.php
+++ app/Jobs/MigrateFilesToR2.php
@@ -13,6 +13,7 @@ class MigrateFilesToR2 implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+     public $timeout = 180;
     /**
      * Create a new job instance.
      */
