1026ca5c62cb0d06b3ae6bc1ac652abe3a6de2f6
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2026-06-10 11:34:00 +0800

    sss

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -11,7 +11,7 @@ class GoogleController extends Controller
 {
     public function redirectToGoogle()
     {
-        return Socialite::driver('google')->redirect();
+        return Socialite::driver('google')->with(['prompt' => 'select_account'])->redirect();
     }
 
     /**
